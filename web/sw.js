06dde6078022a8a960412f849920f00336a02982
onlyhayam <279793297+onlyhayam@users.noreply.github.com>
2026-06-26 12:23:26 +0530

    pwa nuked

--- web/sw.js
+++
@@ -1,8 +0,0 @@
-importScripts(
-  "https://storage.googleapis.com/workbox-cdn/releases/6.0.2/workbox-sw.js",
-);
-
-workbox.routing.registerRoute(
-  ({ requrest }) => request.destination === "image",
-  new workbox.strategies.NetworkFirst(),
-);
